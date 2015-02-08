bb78f07cc35f0696ee3d4abce81a463e20b6dfb9
Robin Åkerström <robin.akerstrom@gmail.com>
2015-02-08 18:14:56 +0100

    Tracks endpoint moved to /tracks

--- app/js/services.js
+++ app/js/services.js
@@ -6,7 +6,7 @@ var services = angular.module('services', ['ngResource']);
 
 services.factory('Track', ['$resource',
   function($resource){
-    return $resource('http://localhost:8080/soundcloud/tracks', {}, {
+    return $resource('http://localhost:8080/tracks', {}, {
       query: {method:'GET', isArray:true}
     });
   }]);
