de3f89385aac4d43fa8c48a0dc5dbef7778406cf
Robin Åkerström <robin.akerstrom@gmail.com>
2015-02-08 18:15:56 +0100

    Implemented dynamic listing of sound cloud tracks and youtube videos from the general track model

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,7 +8,30 @@ controllers.controller('TracksController', ['$scope', 'Track',
   function($scope, Track) {
     $scope.tracks = Track.query();
 
-    $scope.getIframeSrc = function(trackId) {
-      return 'https://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/' + trackId + '&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=true';
+    $scope.getSoundcloudIframeSrc = function(soundcloudTrackId) {
+      return 'https://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/' + soundcloudTrackId + '&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=true';
     };
+
+    $scope.getYouTubeIframeSrc = function(youTubeVideoId) {
+      return 'https://www.youtube.com/embed/' + youTubeVideoId + '?rel=0&autohide=1';
+    };
+
+    $scope.getArtistAndTitle = function(track) {
+      var artistAndTitle = track.artist.name;
+      artistAndTitle += track.featuring ? (" feat. " + track.featuring.name) : "";
+      artistAndTitle += " - ";
+      artistAndTitle += track.title;
+
+      if (track.remixer || track.remixTitle) {
+          artistAndTitle += " ("
+          artistAndTitle += track.remixer ?  (track.remixer.name + " ") : "";
+          artistAndTitle += track.remixTitle ?  track.remixTitle : "";
+          artistAndTitle += ")"
+      }
+
+      artistAndTitle += track.preview ? (" [PREVIEW]") : "";
+      return artistAndTitle;
+    };
+
+
   }]);
